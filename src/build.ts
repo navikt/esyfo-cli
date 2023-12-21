945e8cb940e98e04af0718c7bcad74747ee6f4b4
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2023-12-21 14:55:52 +0100

    Use @navikt/eslint-config-teamsykmelding

--- src/build.ts
+++ src/build.ts
@@ -1,31 +1,32 @@
-import chalk from "chalk";
+import chalk from 'chalk'
 
-import { version } from "../esyfo-cli/package.json";
+import { version } from '../esyfo-cli/package.json'
 
 const result = await Bun.build({
-  entrypoints: ["src/index.ts"],
-  target: "bun",
-  define: {
-    "process.env.COMPILED_BINARY": '"true"',
-  },
-});
+    entrypoints: ['src/index.ts'],
+    target: 'bun',
+    define: {
+        'process.env.COMPILED_BINARY': '"true"',
+    },
+})
 
 if (result.outputs.length > 1) {
-  throw new Error("Expected only one output");
+    throw new Error('Expected only one output')
 }
 
-const [artifact] = result.outputs;
-const outputWriter = Bun.file("./esyfo-cli/bin/ecli").writer();
+const [artifact] = result.outputs
+const outputWriter = Bun.file('./esyfo-cli/bin/ecli').writer()
 
-outputWriter.write("#!/usr/bin/env bun\n");
-outputWriter.write(await artifact.text());
+outputWriter.write('#!/usr/bin/env bun\n')
+outputWriter.write(await artifact.text())
 
-Bun.spawnSync("chmod +x ./esyfo-cli/bin/ecli".split(" "), {
-  stdout: "inherit",
-});
+Bun.spawnSync('chmod +x ./esyfo-cli/bin/ecli'.split(' '), {
+    stdout: 'inherit',
+})
 
+/* eslint-disable no-console */
 console.info(
-  `Built ${version} (${chalk.green(
-    `${(artifact.size / 1024).toFixed(0)}KB`,
-  )}) bytes to ${chalk.yellow("./esyfo-cli/bin/ecli")}`,
-);
+    `Built ${version} (${chalk.green(`${(artifact.size / 1024).toFixed(0)}KB`)}) bytes to ${chalk.yellow(
+        './esyfo-cli/bin/ecli',
+    )}`,
+)
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-console.log("Hello via Bun!");
+/* eslint-disable no-console */
+console.log('Hello via Bun!')
